e110d6ebd5806eee7c4ad86a418718d87ba16757
nuttysunday <shivamghodke10000@gmail.com>
2024-11-29 10:13:56 -0500

    stable version

---
+++ src/lib/supabase.js
@@ -0,0 +1,11 @@
+// lib/supabase.js
+
+import { createClient } from '@supabase/supabase-js';
+
+// Initialize Supabase client with environment variables
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+export default supabase;
